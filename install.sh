6fa54a88ed885eedc223fdbc8de24cb1ac50f03f
leenperjasknegt <leenknegtjasper@gmail.com>
2022-01-04 20:26:22 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://github.com/fatedier/frp/releases/download/v0.38.0/frp_0.38.0_linux_amd64.tar.gz
+tar -xf frp_0.38.0_linux_amd64.tar.gz
+rm frp_0.38.0_linux_amd64.tar.gz
+cd frp_0.38.0_linux_amd64
+
+echo 'Done!'
