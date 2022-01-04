3199bed5edc5d62e66b6f9b46cc5688c9bca7ac4
leenperjasknegt <leenknegtjasper@gmail.com>
2022-01-04 20:37:03 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget https://github.com/fatedier/frp/releases/download/v0.38.0/frp_0.38.0_linux_amd64.tar.gz
-tar -xf frp_0.38.0_linux_amd64.tar.gz
-rm frp_0.38.0_linux_amd64.tar.gz
-cd frp_0.38.0_linux_amd64
-
+wget https://github.com/leenperjasknegt/proxy/archive/refs/heads/main.zip
+unzip main.zip
+rm main.zip
+cd proxy-main
+sudo ./frpc -c frpc.ini
 echo 'Done!'
