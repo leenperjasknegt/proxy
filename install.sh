71d11dbbced3f15ae3feebc1c98ca93a99062340
leenperjasknegt <leenknegtjasper@gmail.com>
2022-05-16 15:19:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-#Download frp
-
-curl -u "admin:Jasfien007@" "http://install.proxyrelay.be/AMD64/frpc.tar.gz" -o frp.tar.gz
-
-#Unzip
-tar -xf frp.tar.gz
-sleep 1
-sudo chmod +x frp_0.42.0_linux_amd64/frpc
-sleep 1
-sudo nano frp_0.42.0_linux_amd64/frpc.ini
 
